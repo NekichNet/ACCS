3f77c3d14c9b3c7026a4d405c14f0b8311564033
NekichNet <102032304+NekichNet@users.noreply.github.com>
2026-01-13 08:40:31 +0500

    Removed doc assigning with no need.

--- accs/Models/Reward.cs
+++ accs/Models/Reward.cs
@@ -6,7 +6,6 @@
 		public string Name { get; set; }
 		public string Description { get; set; } = string.Empty;
 		public string? ImagePath { get; set; } // Путь к картинке на диске
-		public Doc? Doc { get; set; } // Документ о награждении
 		public List<Unit> Units { get; set; } = new List<Unit>();
 	}
 }
--- accs/Models/UnitStatus.cs
+++ accs/Models/UnitStatus.cs
@@ -7,6 +7,5 @@
         public Status Status { get; set; }
         public DateTime Start { get; set; } = DateTime.UtcNow;
         public DateTime End { get; set; }
-        public Doc? Doc { get; set; }
     }
 }

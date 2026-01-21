ffc328a19948de9e5edea56d7964e156b21f15ee
Yupi <youngperchik2404@gmail.com>
2026-01-22 00:26:22 +0500

    implemented LessonTicket #4

---
+++ accs/Models/Tickets/LessonTicket.cs
@@ -0,0 +1,23 @@
+﻿using Discord.WebSocket;
+
+namespace accs.Models.Tickets
+{
+    public class LessonTicket : Ticket
+    {
+        public LessonTicket(SocketGuild guild, ulong authorId, ulong channelId) : base(guild, authorId, channelId)
+        {
+        }
+
+        public override async Task SendWelcomeMessage()
+        {
+            var channel = _guild.GetTextChannel(ChannelDiscordId);
+            if (channel == null)
+                return;
+
+            await channel.SendMessageAsync(
+                "Вы подали запрос на помощь у инструкторов.\n" +
+                "Инструкторский корпус скоро рассмотрит ваш запрос и свяжется с вами."
+            );
+        }
+    }
+}

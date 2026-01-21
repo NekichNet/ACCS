5e49ff2aa79355170c935363f9f152f59e12dd8c
Yupi <youngperchik2404@gmail.com>
2026-01-22 00:36:45 +0500

    implemented ReportTicket #13

---
+++ accs/Models/Tickets/ReportTicket.cs
@@ -0,0 +1,23 @@
+﻿using Discord.WebSocket;
+
+namespace accs.Models.Tickets
+{
+    public class ReportTicket : Ticket
+    {
+        public ReportTicket(SocketGuild guild, ulong authorId, ulong channelId) : base(guild, authorId, channelId)
+        {
+        }
+
+        public override async Task SendWelcomeMessage()
+        {
+            var channel = _guild.GetTextChannel(ChannelDiscordId);
+            if (channel == null)
+                return;
+
+            await channel.SendMessageAsync(
+                "Вы обратились в отдел жалоб клана.\n" +
+                "Военная Полиция скоро рассмотрит ваш запрос и свяжется с вами."
+            );
+        }
+    }
+}

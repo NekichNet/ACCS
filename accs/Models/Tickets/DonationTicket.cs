8f90592ddb8c79d0e87139f3f5d97ee923c227fa
Yupi <youngperchik2404@gmail.com>
2026-01-22 00:32:53 +0500

    implemented DonationTicket #7

---
+++ accs/Models/Tickets/DonationTicket.cs
@@ -0,0 +1,50 @@
+﻿using Discord.WebSocket;
+
+namespace accs.Models.Tickets
+{
+    public class DonationTicket : Ticket
+    {
+        public DonationTicket(SocketGuild guild, ulong authorId, ulong channelId) : base(guild, authorId, channelId)
+        {
+        }
+
+        public override async Task SendWelcomeMessage()
+        {
+            var channel = _guild.GetTextChannel(ChannelDiscordId);
+            if (channel == null)
+                return;
+
+            string requisites = DotNetEnv.Env.GetString("DONATION_REQUISITES",
+                "Реквизиты для пожертвований не найдены.");
+
+            await channel.SendMessageAsync(
+                "Спасибо за желание поддержать клан!\n" +
+                "Вот реквизиты для пожертвования:\n\n" +
+                requisites
+            );
+        }
+
+
+        public override async Task Accept()
+        {
+            var user = _guild.GetUser(AuthorDiscordId);
+            if (user != null)
+            {
+                string friendRoleIdStr = DotNetEnv.Env.GetString("FRIEND_ROLE_ID");
+                if (ulong.TryParse(friendRoleIdStr, out ulong friendRoleId))
+                {
+
+                    var friendRole = _guild.GetRole(friendRoleId);
+
+                    if (friendRole != null)
+                    {
+                        await user.AddRoleAsync(friendRole);
+                    }
+                }
+            }
+
+            Status = TicketStatus.Accepted;
+            await Close();
+        }
+    }
+}

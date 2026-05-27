81a96cdf554175e32ad68bf190fb28f538a78e45
Yupi <youngperchik2404@gmail.com>
2026-05-27 19:00:05 +0500

    add empty controllers

---
+++ accs/Controllers/DocController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace accs.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DocController : ControllerBase
+    {
+    }
+}
---
+++ accs/Controllers/UnitController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace accs.Controllers
+{
+    [Route ("api/[controller]")]
+    [ApiController]
+    public class UnitController : ControllerBase
+    {
+        [HttpGet("unit")]
+        public async Task<IActionResult> GetUnit(
+            [FromQuery] int post,
+            [FromQuery] int rank,
+            [FromQuery] int reward)
+        {
+            return await Task.FromResult(Ok());
+        }
+
+        [HttpPost("unit")]
+        public async Task<IActionResult> CreateUnit(
+            [FromQuery] int post,
+            [FromQuery] int rank,
+            [FromQuery] int reward)
+        {
+            return await Task.FromResult(Ok());
+        }
+
+        [HttpGet("unit/{id}")]
+        public async Task<IActionResult> GetUnitById(
+            [FromRoute] int id)
+        {
+            return await Task.FromResult(Ok());
+        }
+
+        [HttpPatch("unit/{id}")]
+        public async Task<IActionResult> UpdateUnitById(
+            [FromRoute] int id,
+            [FromQuery] int post,
+            [FromQuery] int rank,
+            [FromQuery] int reward)
+        {
+            return await Task.FromResult(Ok());
+        }
+    }
+}
